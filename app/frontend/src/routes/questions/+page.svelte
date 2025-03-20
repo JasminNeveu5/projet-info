0d129175924a6160d5ec31747b1374f18c6434b1
Jasmin Neveu <jasmin.neveu@enelve.ensai.fr>
2025-03-21 00:10:26 +0100

    add: api

--- app/frontend/src/routes/questions/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1> Question 1 </h1>
-<a href = "/questions/1"> Question 1 </a>
-<a href = "/questions/2"> Question 1 </a>
--- app/frontend/src/routes/questions/[questionId]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    let data = $props();
-    $inspect(data);
-</script>
-
-<p>
-    {data.body}
-</p>
