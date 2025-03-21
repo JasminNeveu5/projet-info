6f8a7521929a3bfd9e37c8b2763845e47f85a4a9
Jasmin Neveu <jasmin.neveu@enelve.ensai.fr>
2025-03-21 01:06:55 +0100

    fix: path files & break tie

--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -11,8 +11,11 @@ async def index():
 
 @app.get("/q1_{nb_victoires}")
 async def q1(nb_victoires: int):
-    results = pd.read_csv("~/Documents/1A-ENSAI/projet-info/data/results.csv")
-    drivers = pd.read_csv("~/Documents/1A-ENSAI/projet-info/data/drivers.csv")
+    if nb_victoires < 0:
+        raise HTTPException(status_code=404,
+                            detail="Nombre de victoires invalide")
+    results = pd.read_csv("../../data/results.csv")
+    drivers = pd.read_csv("../../data/drivers.csv")
     return (
         pd.merge(results, drivers, on="driverId")
         .query("position == '1'")
@@ -27,14 +30,27 @@ async def q1(nb_victoires: int):
 
 @app.get("/q2_{annee}")
 async def q2(annee: int):
-    results = pd.read_csv("~/Documents/1A-ENSAI/projet-info/data/results.csv")
-    drivers = pd.read_csv("~/Documents/1A-ENSAI/projet-info/data/drivers.csv")
-    races = pd.read_csv("~/Documents/1A-ENSAI/projet-info/data/races.csv")
-    return pd.merge(
-        pd.merge(results, races[races["year"] == annee], on="raceId")
-        .groupby("driverId")
-        .sum("points")
-        .sort_values("points", ascending=False),
-        drivers,
-        on="driverId",
-    )[["forename", "surname", "points"]].to_json(orient="records")
+    if annee < 1950 or annee > 2024:
+        raise HTTPException(status_code=404, detail="Année invalide")
+    results = pd.read_csv("../../data/results.csv")
+    drivers = pd.read_csv("../..//data/drivers.csv")
+    races = pd.read_csv("../../data/races.csv")
+    races_year = pd.merge(results, races[races["year"] == annee], on="raceId")
+    races_year["positionOrder"] = races_year["positionOrder"].astype(int)
+    points_wins = (
+        races_year.groupby("driverId")
+        .agg(
+            points=("points", "sum"),
+            wins=("positionOrder", lambda x: (x == 1).sum()),
+            second_places=("positionOrder", lambda x: (x == 2).sum()),
+            third_places=("positionOrder", lambda x: (x == 3).sum()),
+        )
+        .reset_index()
+    )
+    sorted_drivers = points_wins.sort_values(
+        by=["points", "wins", "second_places", "third_places"],
+        ascending=[False, False, False, False],
+    )
+    return pd.merge(sorted_drivers, drivers, on="driverId")[
+        ["forename", "surname", "points"]
+    ].to_json(orient="records")
