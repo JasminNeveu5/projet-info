f74a2568ea4ed100e3bd6b3aaacce0c3ff614aad
Jasmin Neveu <jasmin.neveu@enelve.ensai.fr>
2025-03-21 21:04:08 +0100

    feat: best lap

--- app/api/controller/pilote_controller.py
+++ app/api/controller/pilote_controller.py
@@ -1,12 +1,28 @@
 from fastapi import HTTPException, APIRouter
 from api.services.pilote_services import DefaultQuery
+import pandas as pd
+
 
 router = APIRouter()
 
 
-@router.get("/q1_{nb_victoires}")
-async def q1(nb_victoires: int):
+@router.get("/nombre_victoire_{nb_victoires}")
+async def nombre_victoire(nb_victoires: int):
     if nb_victoires < 0:
-        raise HTTPException(status_code=404,
-                            detail="Nombre de victoire Invalide")
-    return DefaultQuery.q1(nb_victoires)
+        raise HTTPException(status_code=404, detail="Nombre de victoire Invalide")
+    return DefaultQuery.nombre_victoire(nb_victoires)
+
+
+@router.get("/classement_{annee}")
+async def classement(annee: int):
+    if annee < 1950 or annee > 2024:
+        raise HTTPException(status_code=404, detail="Annee invalide")
+    return DefaultQuery.classement(annee)
+
+
+@router.get("/meilleur_tour_{localisation}")
+async def meilleur_tour(localisation: str):
+    circuits = pd.read_csv("../../data/circuits.csv")
+    if not circuits["location"].isin([localisation]).any():
+        raise HTTPException(status_code=404, detail="Invalide location")
+    return DefaultQuery.meilleur_temps(localisation)
