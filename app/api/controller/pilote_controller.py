a95ce3a55f71c4d892c83baaa289919886543e2a
Victor Jean <victor.jean@eleve.ensai.fr>
2025-03-28 12:12:56 +0100

    fix: re-added api function

--- app/api/controller/pilote_controller.py
+++ app/api/controller/pilote_controller.py
@@ -42,3 +42,28 @@ async def casse_constructeur(constructeur: str):
     if not constructeurs["name"].isin([constructeur]).any():
         raise HTTPException(status_code=404, detail="Constructeur invalide")
     return DefaultQuery.casse_constructeur(constructeur)
+
+
+@router.get("/temps_dernier_qualif_{circuit}_{annee}")
+async def temps_max_qualif(circuit: str, annee: str):
+    """
+    Retrieves the maximum qualifying time for a given circuit and year. This function queries the
+    data from predefined circuit and race data files. If the specified circuit reference or year
+    is invalid, an HTTP 404 error is raised.
+
+    :param circuit: The reference identifier for the circuit, provided as a string. It must match
+        one of the existing circuit references in the circuit data file.
+    :param annee: The year of the qualifying record to retrieve, provided as a string. It must
+        match one of the years in the race data file.
+    :return: The maximum qualifying time for the specified circuit and year obtained from the
+        data source.
+    :rtype: str
+    :raises HTTPException: Raised with a 404 status code if the given circuit or year is invalid.
+    """
+    circuits = pd.read_csv(f"{DATA_DIR}/circuits.csv")
+    races = pd.read_csv(f"{DATA_DIR}/races.csv")
+    if not circuits["circuitRef"].isin([circuit]).any():
+        raise HTTPException(status_code=404, detail="Invalide circuit")
+    if not races["year"].isin([int(annee)]).any():
+        raise HTTPException(status_code=404, detail="Invalide annee")
+    return DefaultQuery.temps_min_qualif_annee(circuit, annee)
