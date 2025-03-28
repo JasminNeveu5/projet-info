a274ac15b96756c9a7e8fd482a63007e0d39eaa7
Justine Oliviero <id2768@eleve.ensai.fr>
2025-03-28 09:51:05 +0100

    feat: mon travail sur les questions

--- questions_obligatoires/Q_ju_temps_moyen_pit_stops.py
+++ questions_obligatoires/Q_ju_temps_moyen_pit_stops.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 # Obtenir le temps moyen passé au pit-stops par année
 pit_stops = pd.read_csv('C:/DEVOIRS/ENSAI1A/projet-info/data/pit_stops.csv')
@@ -12,21 +11,21 @@ temps_moyen_pit_stops = jointure.groupby('year')['milliseconds'].mean()
 
 
 # Graphique du temps moyen passé au pit-stops par année
-plt.axhline(y=60000,color='gray',linestyle='--')
-plt.axhline(y=120000,color='gray',linestyle='--')
-plt.axhline(y=180000,color='gray',linestyle='--')
-plt.text(2011, 60000, '1min', backgroundcolor = 'grey')
-plt.text(2011, 120000, '2min', backgroundcolor = 'grey')
-plt.text(2011, 180000, '3min', backgroundcolor = 'grey')
+plt.axhline(y=60000, color='gray', linestyle='--')
+plt.axhline(y=120000, color='gray', linestyle='--')
+plt.axhline(y=180000, color='gray', linestyle='--')
+plt.text(2011, 60000, '1min', backgroundcolor='grey')
+plt.text(2011, 120000, '2min', backgroundcolor='grey')
+plt.text(2011, 180000, '3min', backgroundcolor='grey')
 temps_moyen_pit_stops.plot(
-    x = 'year',
-    y = 'milliseconds',
-    marker = "o",
-    title = "Temps moyen passé au pit-stops par année",
-    xlabel = 'Année',
-    ylabel = 'Millisecondes',
-    ylim = (0,230000),
-    color = 'orange'
+    x='year',
+    y='milliseconds',
+    marker="o",
+    title="Temps moyen passé au pit-stops par année",
+    xlabel='Année',
+    ylabel='Millisecondes',
+    ylim=(0, 230000),
+    color='orange'
 )
 
 
