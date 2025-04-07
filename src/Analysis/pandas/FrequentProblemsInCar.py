597321487bf03d396b5976e3e70f50e4d084af5f
Victor Jean <victor.jean@eleve.ensai.fr>
2025-04-07 10:23:41 +0200

    feat: added function that is more versatile than fixed code in FrequentProblemsInCar.py

--- src/Analysis/pandas/FrequentProblemsInCar.py
+++ src/Analysis/pandas/FrequentProblemsInCar.py
@@ -23,7 +23,9 @@ total_counts = results_x_constructor.groupby('constructorId').size().reset_index
 status_counts = pd.merge(status_counts, total_counts, on='constructorId')
 status_counts['proportion'] = status_counts['count'] / status_counts['total_count']
 
+
 # === Problèmes les plus fréquents par constructeur ===
+
 
 
 # Trie par 'constructorId' et 'count' (desc), et sélection des 10 principaux status ID par constructeur
@@ -36,6 +38,7 @@ status_counts_per_constructor = pd.merge(status_counts_per_constructor, f1_const
 
 # status_counts_per_constructor.drop(["statusId","constructorId"], axis=1, inplace=True)
 print(status_counts_per_constructor.head())
+
 
 # === Constructeurs avec le plus de problèmes ===
 
@@ -54,3 +57,47 @@ failures_per_constructor['failure_proportion'] = failures_per_constructor['count
 failures_per_constructor_sorted = failures_per_constructor.sort_values(by='failure_proportion', ascending=False)
 
 print(failures_per_constructor_sorted[['name', 'failure_proportion']])
+
+
+def get_status_code_occurrences(status_code, manufacturer=None):
+    """
+    Retrieve the occurrences of a given statusCode, optionally filtered by car manufacturer.
+
+    :param status_code: (int) The statusCode to filter results by.
+    :param manufacturer: (str, optional) The car manufacturer to filter results.
+                         If not provided, results for all manufacturers will be included.
+    :return: (pd.DataFrame) A DataFrame containing the filtered data with columns:
+             "statusId", "number of occurrence", "proportion of occurrences", "car manufacturer".
+    
+    Examples:
+    >>> get_status_code_occurrences(5).head()
+        statusId  number of occurrence  proportion of occurrences car manufacturer
+    0          5                   126                   0.066351          McLaren
+    10         5                     4                   0.028571       BMW Sauber
+    19         5                    81                   0.049031         Williams
+    29         5                    52                   0.066074          Renault
+    39         5                    16                   0.029851       Toro Rosso
+    >>> get_status_code_occurrences(5, manufacturer="Ferrari")
+        statusId  number of occurrence  proportion of occurrences car manufacturer
+    49         5                   166                   0.068737          Ferrari
+
+    """
+    # Filter data based on the given statusCode
+    filtered_data = status_counts_per_constructor[status_counts_per_constructor["statusId"] == status_code]
+
+    # If a manufacturer is provided, filter further by its name
+    if manufacturer:
+        filtered_data = filtered_data[filtered_data["name"].str.lower() == manufacturer.lower()]
+
+    # Rename columns for consistency
+    result = filtered_data.rename(
+        columns={
+            "statusId": "statusId",
+            "count": "number of occurrence",
+            "proportion": "proportion of occurrences",
+            "name": "car manufacturer"
+        }
+    )
+
+    # Select necessary columns
+    return result[["statusId", "number of occurrence", "proportion of occurrences", "car manufacturer"]]
