684b146305dead61c92e87d1fd0067ae66c61703
Justine Oliviero <id2768@eleve.ensai.fr>
2025-04-08 12:38:07 +0200

    fix old pb

--- src/Analysis/pandas/AverageTimePitStop.py
+++ src/Analysis/pandas/AverageTimePitStop.py
@@ -1,26 +1,29 @@
 import pandas as pd
-#from src.Common.utils import convert_to_human_readable, human_readable_formatter
-#import matplotlib.pyplot as plt
-#from matplotlib.ticker import FuncFormatter
+from options.config import DATA_DIR
+from src.Common.utils import convert_to_human_readable, human_readable_formatter
+from matplotlib.ticker import FuncFormatter
+import matplotlib.pyplot as plt
 
 
 # Import et création de la table utilisée
-
-
 
 pit_stops = pd.read_csv(f"{DATA_DIR}/pit_stops.csv")
 races = pd.read_csv(f"{DATA_DIR}/races.csv")
 jointure = pd.merge(pit_stops, races, on="raceId", how="left")
 
 
-def temps_moyen_pit_stops(circuit: str) -> float:
-    return jointure.groupby('year')['milliseconds'][circuit].mean()
+def AverageTimePitStop(circuit: str) -> float:
+    return jointure.groupby('year').agg('milliseconds').mean()
+    #return jointure.groupby('year')['milliseconds'][circuit].mean()
 
 
 # Graphique
 
 
 name = 'Australian Grand Prix'
+print(AverageTimePitStop(name))
+
+
 temps_moyen_pit_stops_readable = AverageTimePitStop(name).map(convert_to_human_readable)
 
 
@@ -29,7 +32,7 @@ AverageTimePitStop(name).plot(
     x='year',
     y='milliseconds',
     marker="o",
-    ylim=(0, 1000000),
+    ylim=(0, 240000),
     color='orange'
 )
 plt.gca().yaxis.set_major_formatter(FuncFormatter(human_readable_formatter))
