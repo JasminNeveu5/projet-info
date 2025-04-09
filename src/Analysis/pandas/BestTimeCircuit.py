2ff1794b8c5a8486f02a0d3b275a4efcee79e0e7
Jasmin Neveu <jasmin.neveu@enelve.ensai.fr>
2025-04-09 12:22:11 +0200

    init

---
+++ src/Analysis/pandas/BestTimeCircuit.py
@@ -0,0 +1,18 @@
+from options.config import DATA_DIR
+import pandas as pd
+from src.Common.utils import convert_to_human_readable
+from src.Model.Circuit import Circuit
+
+def bestTimeCircuit(localisation: str):
+    if not isinstance(localisation, str):
+        raise TypeError("localisation doit être de type str")
+
+    lap_times = pd.read_csv(f"{DATA_DIR}/lap_times.csv")
+    circuits = pd.read_csv(f"{DATA_DIR}/circuits.csv")
+    races = pd.read_csv(f"{DATA_DIR}/races.csv")
+
+    meilleur_temps_course = lap_times.groupby('raceId')['milliseconds'].min().reset_index()
+    m2 = meilleur_temps_course.merge(races, on='raceId').groupby("circuitId")['milliseconds'].min().reset_index().merge(circuits, on='circuitId')
+    m2["meilleur_ever"] = m2["milliseconds"].apply(convert_to_human_readable)
+    f = m2[m2["location"].str.contains(localisation, case=False)]
+    return Circuit(name = f["name"].values[0], country = f["country"].values[0], location = f["location"].values[0], best_time = f["meilleur_ever"].values[0])
