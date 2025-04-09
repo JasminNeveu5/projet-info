39dd5d90f2ca1777f1954b7b828370014671da4e
Victor Jean <victor.jean@eleve.ensai.fr>
2025-04-09 12:05:10 +0200

    feat: documentation

--- src/Analysis/pandas/MostDamagedDriver.py
+++ src/Analysis/pandas/MostDamagedDriver.py
@@ -3,10 +3,21 @@ import pandas as pd
 from options.config import DATA_DIR
 
 
-def most_damaged_driver(nombre_courses_minimum = 0):
-    if not isinstance(nombre_courses_minimum, int):
-        raise TypeError("nombre_courses_minimum doit être de type int")
+def most_damaged_driver(nombre_courses_minimum):
+    """
+    Identifies the drivers with the highest accident ratio based on their number of accidents
+    relative to their total races. The function filters drivers who have participated in at
+    least a specified minimum number of races.
 
+    :param nombre_courses_minimum: The minimum number of races a driver must have participated in
+                                   to be considered in the analysis.
+    :type nombre_courses_minimum: int
+    :return: A list of Driver objects containing details of the most accident-prone drivers,
+             filtered based on the minimum number of races. Each object includes driver ID,
+             forename, surname, nationality, number of accidents, total number of races,
+             and accident ratio.
+    :rtype: list[Driver]
+    """
     results = pd.read_csv(f"{DATA_DIR}/results.csv")
     drivers = pd.read_csv(f"{DATA_DIR}/drivers.csv")
 
@@ -35,5 +46,3 @@ def most_damaged_driver(nombre_courses_minimum = 0):
             )
         )
     return result
-
-print(most_damaged_driver(300)[0])
