a528f711dc09be2ca8c16e1663ba9f0bacb7ba20
Justine Oliviero <id2768@eleve.ensai.fr>
2025-04-11 10:51:43 +0200

    feat: liste contructeurs en sortie

--- src/Analysis/pandas/BestConstructors.py
+++ src/Analysis/pandas/BestConstructors.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from options.config import DATA_DIR
+from src.Model import Constructor
 import matplotlib.pyplot as plt
 
 
@@ -10,17 +11,35 @@ races = pd.read_csv(f'{DATA_DIR}/races.csv')
 
 jointure = pd.merge(constructor_standings, constructors, on='constructorId', how='left')
 jointure = pd.merge(jointure, races, on='raceId', how='left')
-jointure = jointure[['name_x', 'year', 'wins']]
+jointure = jointure[['name_x', 'year', 'wins','nationality']]
 
 
 # Fonction
 
 
 def BestConstructors(wanted_year):
-    result = jointure[jointure['year'] == wanted_year].groupby('name_x').apply(np.sum, axis=0).sort_values('wins', ascending=False) # agg({'wins':sum}))
+    result = jointure[jointure['year'] == wanted_year].groupby('name_x').apply(np.sum, axis=0).sort_values('wins', ascending=False)
     result['name'] = result.index
+    result = result[['name', 'nationality', 'wins']]
+    print(result)
+    print(result.columns)
+
+    BestConstructorsList = []
+
+    for row in result.iterrows():
+        print(row[0], row[1], len(row))
+
+        BestConstructorsList.append(
+            Constructor(
+                name=row['name'],
+                nationality=row['natinality'],
+                nombre_victoire=row['wins'],
+            )
+        )
+
     return result
 
+print(BestConstructors(2003))
 
 # Exemple pris
 
